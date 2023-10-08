e2f1bd6e1f5027a0ba4565e217c9e969af28384a
pankaj-kb <pankajb2601@gmail.com>
2023-10-08 17:47:03 +0530

    added social route api for insta

--- index.js
+++ index.js
@@ -7,10 +7,25 @@ const PORT = 4000 || process.env.PORT
 const path = require('path');
 
 app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'Hello.html'))
-    res.send("<h1> Hello World </h1>")
+    res.sendFile(path.join(__dirname, 'Hello.html'))
+
+    // res.status(201).send("<h1> Hello World </h1>")
 })
 
+app.get('/api/v1/instagram', (req, res) => {
+    let pankaj = {
+        name: "Pankaj",
+        followers: "100",
+        following: "100",
+        posts: "5",
+        date: Date.now()
+    }
+    res.status(200).send(pankaj)
+})
+
+
+
+
 app.listen(PORT, () => {
-    console.log(`server is running at ${PORT}`)
+    console.log(`server is running at localhost:${PORT}`)
 });
