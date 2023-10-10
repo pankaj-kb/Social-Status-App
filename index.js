a9fae352778e01b654ec19d29ece80e6c67a0e5a
pankaj-kb <pankajb2601@gmail.com>
2023-10-10 15:10:44 +0530

    fixing date issue

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const PORT = 4000 || process.env.PORT
 
 const path = require('path');
 
+const format = require('date-format')
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'Hello.html'))
 
@@ -15,6 +17,43 @@ app.get('/', (req, res) => {
 app.get('/api/v1/instagram', (req, res) => {
     let pankaj = {
         name: "Pankaj",
+        username: "pankajkb",
+        followers: "100",
+        following: "1010",
+        posts: "5",
+        date: format.asString("hh:mm:ss", new Date())
+    }
+    res.status(200).send(pankaj)
+})
+
+app.get('/api/v1/linkedin', (req, res) => {
+    let pankaj = {
+        name: "Pankaj",
+        username: "pankajkb",
+        followers: "1000",
+        following: "900",
+        posts: "1",
+        date: Date.now()
+    }
+    res.status(200).send(pankaj)
+})
+
+app.get('/api/v1/x', (req, res) => {
+    let pankaj = {
+        name: "Pankaj",
+        username: "pankajkb",
+        followers: "1010",
+        following: "100",
+        posts: "5",
+        date: Date.now()
+    }
+    res.status(200).send(pankaj)
+})
+
+app.get('/api/v1/facebook', (req, res) => {
+    let pankaj = {
+        name: "Pankaj",
+        username: "pankajkb",
         followers: "100",
         following: "100",
         posts: "5",
